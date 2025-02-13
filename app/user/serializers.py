b715df54f40014e25cba0f43020ee3a0d9ba769a
adrianpietrzak <a.pietrzak@smartsoftsolutions.pl>
2025-02-13 15:50:40 +0100

    Add user authentication with token-based login
    Introduce `AuthTokenSerializer` and `CreateTokenView` to support user authentication via tokens. Update URLs to include a new endpoint for generating tokens and enable `rest_framework.authtoken` in settings. These changes facilitate secure user login functionality.

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 
 """
 Serializers for the user API
@@ -17,3 +17,22 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create and return user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user authentication object"""
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(request=self.context.get('request'), username=email, password=password)
+        if not user:
+            msg = 'Unable to authenticate with provided credentials'
+            raise serializers.ValidationError(msg, code='authentication')
+        attrs['user'] = user
+        return attrs
