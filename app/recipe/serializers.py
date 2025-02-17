fc4c634a59abeebc9ea57fbdd5d35c363b85885c
adrianpietrzak <a.pietrzak@smartsoftsolutions.pl>
2025-02-17 15:30:43 +0100

    Add recipe management API with tests and serializers
    Introduce API endpoints for managing recipes, including listing and filtering by user. Implement `RecipeViewSet`, serializers, and corresponding URLs. Add unit tests to verify API behavior and enforce authentication.

---
+++ app/recipe/serializers.py
@@ -0,0 +1,13 @@
+
+
+from core.models import Recipe
+from rest_framework import serializers
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipe objects"""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
