d5a04ab17dd4c57e407f077164f0fa024e4665e4
adrianpietrzak <a.pietrzak@smartsoftsolutions.pl>
2025-02-19 19:33:55 +0100

    Add Tag API support with serializer, viewset, and endpoint
    Introduced functionality to list tags with a new TagSerializer and TagViewSet. Updated the router to include a 'tags' endpoint and ensured it only retrieves tags for the authenticated user. This enhances the API by enabling tag management alongside recipes.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,6 @@
 
 
-from core.models import Recipe
+from core.models import Recipe, Tag
 from rest_framework import serializers
 
 
@@ -17,3 +17,11 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tag objects"""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
