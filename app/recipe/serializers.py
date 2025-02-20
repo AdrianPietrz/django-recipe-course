7726c1e1822c7f1b663ab4df69c61aa8b1d6069f
adrianpietrzak <a.pietrzak@smartsoftsolutions.pl>
2025-02-20 19:48:07 +0100

    Add tags support to recipe serializer
    Introduced a `TagSerializer` and updated `RecipeSerializer` to handle tags, allowing recipes to include associated tags. The `create` method was enhanced to support the creation and assignment of tags to recipes. This change improves the flexibility of managing recipe-tag relationships.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -3,21 +3,6 @@
 from core.models import Recipe, Tag
 from rest_framework import serializers
 
-
-class RecipeSerializer(serializers.ModelSerializer):
-    """Serializer for recipe objects"""
-
-    class Meta:
-        model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link']
-        read_only_fields = ['id']
-
-class RecipeDetailSerializer(RecipeSerializer):
-    """Serializer for recipe detail objects"""
-
-    class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
-
 class TagSerializer(serializers.ModelSerializer):
     """Serializer for tag objects"""
 
@@ -25,3 +10,31 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ['id', 'name']
         read_only_fields = ['id']
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipe objects"""
+
+    tags = TagSerializer(many=True, required=False)
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
+        read_only_fields = ['id']
+
+    def create(self, validated_data):
+        """Create a new recipe"""
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(user=user, **tag)
+            recipe.tags.add(tag_obj)
+        return recipe
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail objects"""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
+
